fbaf23baff2980fec0eeb20e6564a76763fe3f05
BrianVillage <135732534+BrianV123@users.noreply.github.com>
2025-11-30 23:27:57 -0600

    changes to Main Menu
    Added background music and button growth hover effects.

--- SPACE SPACE PIRATES/Assets/Scripts/Managers/MainMenuManager.cs
+++ SPACE SPACE PIRATES/Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,17 +1,40 @@
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class MainMenuManager : MonoBehaviour
+public class MainMenuManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
-public void startGame()
+    public float buttonGrowth = 1.5f; //How much the button grows when cursor hovers it.
+
+    public Vector3 ogButtonSize; //Stores OG button size.
+
+    void Start()
     {
-        SceneManager.LoadSceneAsync("Main Scene");
+        //Stores OG button size as the size it started as. 
+        ogButtonSize = transform.localScale;
     }
 
-public void exitGame()
+    public void startGame()
+        {
+            SceneManager.LoadSceneAsync("Main Scene");
+        }
+
+    public void exitGame()
+        {
+            Application.Quit();
+        }
+
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        Application.Quit();
+        Debug.Log("We have entered button!");
+        transform.localScale = ogButtonSize * buttonGrowth; //scales button by ButtonGrowth
     }
 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("We have exited button!");
+        transform.localScale = ogButtonSize; //scales button back to OG size
+    }
 }
